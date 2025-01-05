dfe30525fc94a4046d65ce3d20c091694ec0d970
Ankur Patel <ankurapatela@gmail.com>
2025-01-05 14:38:15 +0530

    Adding compiler for compiling the codes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,8 @@ const App = () => {
   const [copySuccess, setCopySuccess] = useState("");
   const [users, setUsers] = useState([]);
   const [typing, setTyping] = useState("");
+  const [outPut, setOutput] = useState("");
+  const [version, setVersion] = useState("*");
 
   useEffect(() => {
     socket.on("userJoined", (users) => {
@@ -33,11 +35,16 @@ const App = () => {
       setLanguage(newLanguage);
     });
 
+    socket.on("codeResponse", (response) =>{
+      setOutput(response.run.output)
+    });
+
     return () => {
       socket.off("userJoined");
       socket.off("codeUpdate");
       socket.off("userTyping");
       socket.off("languageUpdate");
+      socket.off("codeResponse");
     };
   }, []);
 
@@ -87,6 +94,10 @@ const App = () => {
     setLanguage(newLanguage);
     socket.emit("languageChange", { roomId, language: newLanguage });
   };
+
+  const runCode = () => {
+    socket.emit("compileCode",{code, roomId, language, version});
+  }
 
   const saveToFile = () => {
     if (!code) {
@@ -163,7 +174,7 @@ const App = () => {
 
       <div className="editor-wrapper">
         <Editor
-          height="100%"
+          height="60%"
           language={language}
           value={code}
           onChange={handleCodeChange}
@@ -173,6 +184,11 @@ const App = () => {
             fontSize: 14,
           }}
         />
+        <button className="run-btn" onClick={runCode}>Execute</button>
+        <textarea className="output-console" value={outPut} readOnly
+        placeholder="Output will appear here..."
+        />
+        
       </div>
     </div>
   );
